a325c418b7ce3f238b3a134ba8891fcebb579a4a
MKRoet <mauriceroet@hotmail.co>
2017-11-21 14:21:24 +0100

    Met comments

--- classesnew.py
+++ classesnew.py
@@ -1,22 +1,31 @@
 from abc import ABCMeta, abstractmethod
+from randomize import *
+
+# -------------------------------------------------------------------------
+
+# Overkoepelende klasse voor de objecten in het veld.
 class House(object):
 
+    # Zorgt ervoor dat er niet een 'House' geinitieerd kan worden.
     __metaclass__ = ABCMeta
 
+    # Vaste kenmerken die ieder huis bezit.
     base_sale_price = 0
     width = 0
     depth = 0
     base_detached = 0
 
+    # Aangeven op welke plek het huis moet komen.
     def __init__(self, x_coordinate, y_coordinate):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
 
-
+    # Geeft een string terug met het type huis (familyhome, bungalow of maison).
     @abstractmethod
     def house_type(self):
         pass
 
+# De verschillende subklasses die in het veld komen.
 class Familyhome(House):
 
     base_sale_price = 285000
@@ -36,6 +45,7 @@ class Bungalow(House):
 
     def house_type(self):
         return 'bungalow'
+
 
 class Maison(House):
 
