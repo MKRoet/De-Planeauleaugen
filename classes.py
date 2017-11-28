66ac10d9f408ce0979c12584818b9a7c75a05b79
Chantal Stangenberger <cm.stangenberger@gmail.com>
2017-11-28 14:10:37 +0100

    random functie werkend

--- classes.py
+++ classes.py
@@ -4,28 +4,24 @@ import random
 
 class House(object):
 
-    # Zorgt ervoor dat er niet een 'House' geinitieerd kan worden.
     __metaclass__ = ABCMeta
 
-    # Vaste kenmerken die ieder huis bezit.
     base_sale_price = 0
     width = 0
     depth = 0
     base_detached = 0
 
-    def __init__(self):
-        self.x_coordinate = random.randint(0,180)
-        self.y_coordinate = random.randint(0,160)
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    # Geeft een string terug met het huistype
     @abstractmethod
     def house_type(self):
         pass
 
 class Familyhouse(House):
-
-    def __init__(self):
-        House.__init__(self)
+    # def __init__(self):
+    #     House.__init__(self)
 
     base_sale_price = 285000
     width = 8
@@ -37,8 +33,8 @@ def house_type(self):
 
 class Bungalow(House):
 
-    def __init__(self):
-        House.__init__(self)
+    # def __init__(self):
+    #     House.__init__(self)
 
     base_sale_price = 399000
     width = 10
@@ -48,11 +44,10 @@ class Bungalow(House):
     def house_type(self):
         return 'bungalow'
 
-
 class Maison(House):
 
-    def __init__(self):
-        House.__init__(self)
+    # def __init__(self):
+    #     House.__init__(self)
 
     base_sale_price = 610000
     width = 11
@@ -64,8 +59,8 @@ class Maison(House):
 
 class Water(House):
 
-    def __init__(self):
-        House.__init__(self)
+    # def __init__(self):
+    #     House.__init__(self)
 
     width = 48
     depth = 120
--- plattegrondMatplotlib.py
+++ plattegrondMatplotlib.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
-from classesnew import *
+from classes import *
+import math
 
 #-----------------------------------------------------------------------------
 
@@ -23,73 +24,49 @@ major_ticksx = np.arange(0, 190, 10)
 ax.set_yticks(major_ticksy)
 ax.set_xticks(major_ticksx)
 
-# verschillende soorten huizen
-fh = Familyhouse()
-bgl = Bungalow()
-ms = Maison()
-wtr = Water()
+# creeërt een list
+house_lists = []
 
-list = []
-
+# vraagt om welk type plattegrond
 total_buildings = int(input("Hoeveel huizen moeten er op de plattegrond? (20, 40, 60)\n"))
-if total_buildings != 20: #40 en 60 moeten erbij
+if not total_buildings in [20, 40, 60]:
     print("Het aantal huizen moet 20, 40 of 60 zijn")
     exit()
 
+# berekent het percentage familyhome, bungalow en maison van totaal aantal huizen
 fh_amount = float(total_buildings) * 0.6
 bgl_amount = float(total_buildings) * 0.25
 ms_amount = float(total_buildings) * 0.15
 
+# zorgt dat de verschillende typen huizen worden weergeven als lettercode
 fh_list = ['fh'] * int(fh_amount)
 bgl_list = ['bgl'] * int(bgl_amount)
 ms_list = ['ms'] * int(ms_amount)
 
-list.append(fh_list + bgl_list + ms_list)
+# voegt alle huizen toe aan de list
+house_lists.extend(fh_list + bgl_list + ms_list)
+print(house_lists)
 
-for i in range int(fh_amount):
-    if 'fh':
-        temp = patches.Rectangle((fh.x_coordinate, fh.y_coordinate),fh.width, fh.depth)
-        ax.add_patch(temp)
+for list in house_lists:
 
-# # # creëert eengezinswoning
-# # ax.add_patch(
-# #     patches.Rectangle(
-# #         (fh.x_coordinate, fh.y_coordinate),
-# #         fh.width, fh.depth,
-# #         )
-# # ) for i in range()
-#
-# # creëert bungalow
-# for g in [
-#     patches.Rectangle(
-#         (bgl.x_coordinate, bgl.y_coordinate),
-#         bgl.width, bgl.depth,
-#         hatch=pattern2[i],
-#         facecolor="pink"
-#     )
-# ]:
-#     ax.add_patch(g)
-#
-# # creëert maison
-# for q in [
-#     patches.Rectangle(
-#         (ms.x_coordinate, ms.y_coordinate),
-#         ms.width, ms.depth,
-#         hatch=pattern3[i],
-#         facecolor="yellow"
-#     ) for i in range(len(pattern3))
-# ]:
-#     ax.add_patch(q)
-#
-# # creëert water
-# for t in [
-#     patches.Rectangle(
-#         (wtr.x_coordinate, wtr.y_coordinate),
-#         wtr.width, wtr.depth,
-#         fill="blue"
-#     )
-# ]:
-#     ax.add_patch(t)
+    if list == 'fh':
+        house_type = Familyhouse
+        color = 'green'
+
+    if list == 'bgl':
+        house_type = Bungalow
+        color = 'yellow'
+
+    if list == 'ms':
+        house_type = Maison
+        color = 'pink'
+
+    x = random.randint(0,180)
+    y = random.randint(0,160)
+
+
+    temp = patches.Rectangle((x,y), house_type.width, house_type.depth, color=color)
+    ax.add_patch(temp)
 
 plt.grid()
 plt.show()
