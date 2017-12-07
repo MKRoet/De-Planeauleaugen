80c190db617288aa21846531d72b43ce01d25100
Chantal Stangenberger <cm.stangenberger@gmail.com>
2017-12-07 13:41:42 +0100

    update

--- Datastructuur/main.py
+++ Datastructuur/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
-from classes import *
+from houseclasses import *
 from functions import *
 
 #-----------------------------------------------------------------------------
